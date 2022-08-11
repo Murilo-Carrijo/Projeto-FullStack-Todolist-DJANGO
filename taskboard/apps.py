77b41839fcf923777faae738674bf78db0686a69
murilo <murilocarrijoadm@gmail.com>
2022-08-11 20:34:26 -0300

    chore: testando a configuração do projeto - Olá mundo na tela

--- taskboard/apps.py
+++ taskboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TaskbordConfig(AppConfig):
+class TaskboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "taskbord"
+    name = "taskboard"
